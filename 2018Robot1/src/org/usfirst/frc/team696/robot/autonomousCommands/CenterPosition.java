63421df63c120bcb4928c2610ba1d9f4bd60d273
clonetron <jgonza1427@gmail.com>
2018-01-12 15:10:59 -0800

    Added more to centerPosition AutoCommand
    Added more experimental commands ex. AddSequential.

--- 2018Robot1/src/org/usfirst/frc/team696/robot/autonomousCommands/CenterPosition.java
+++ 2018Robot1/src/org/usfirst/frc/team696/robot/autonomousCommands/CenterPosition.java
@@ -1,6 +1,8 @@
 package org.usfirst.frc.team696.robot.autonomousCommands;
 
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.PWMTalonSRX;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import org.usfirst.frc.team696.robot.commands.DriveCommand;
@@ -16,8 +18,24 @@ public class CenterPosition extends CommandGroup {
 //          addSequential(new DriveCommand(1, 1, 1, 1), 1);
             addSequential(new DriveCommand(90, -30, 1, 0.05), 4);
             addSequential(new DriveCommand(20, 0, 0.75, 0.05), 4);
+            // Drop the object
+            addSequential(new DriveCommand(-20, -90, 0.75, 0.05), 4);
+            addSequential(new DriveCommand( 30, 0, 0.75, 0.05), 4);
+            addSequential(new DriveCommand( 90, 0, 0.75, 0.05), 4);
+            addSequential(new DriveCommand( 250, 0 , 0.75, 0.1), 4 );
+            addSequential(new DriveCommand( 20, 0, 20), 4);
+
+
 
         }
+        /*
+        if(gameData.charAt(0) == 'R' && gameData.charAt(1) == 'R');
+
+        if(gameData.charAt(0) == 'R' && gameData.charAt(1) == 'L');
+
+        if(gameData.charAt(0) == 'L' && gameData.charAt(1) == 'L');
+
+         */
 
     }
 
