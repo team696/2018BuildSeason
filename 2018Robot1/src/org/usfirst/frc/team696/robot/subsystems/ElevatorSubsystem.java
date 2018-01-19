6f29a0cab019360276cf6e8fe646ce5f77929e98
Ismail Hasan <ismailhasan0132@gmail.com>
2018-01-19 10:09:46 -0800

    Implemented move function for elevator.
    In this iteration of the first robot project, we've added the code to enable the movement of the elevator. We have preset positions, and as of now, no manual movement mode.

--- 2018Robot1/src/org/usfirst/frc/team696/robot/subsystems/ElevatorSubsystem.java
+++ 2018Robot1/src/org/usfirst/frc/team696/robot/subsystems/ElevatorSubsystem.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc.team696.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj.command.Subsystem;
@@ -10,58 +11,124 @@ import org.usfirst.frc.team696.robot.utilities.PID;
 public class ElevatorSubsystem extends Subsystem {
 
     /*
-    motor controllers
+    Motor Controllers
      */
-    TalonSRX motor1;
-    TalonSRX motor2;
-    TalonSRX motor3;
-    TalonSRX motor4;
+    TalonSRX leftElevator;
+    TalonSRX rightElevator;
 
     /*
-    PID values
+    PID Values
      */
     public static double kP = 0;
     public static double kI = 0;
     public static double kD = 0;
     public static double kF = 0;
-    public static PID pid;
 
     public static int pidIdx = 0;
 
     /*
     Preset
      */
+
+    public static String movePos;
+
     public static double groundTarget = 0;
+//    public static boolean groundMove = false;
+
+
     public static double switchTarget = 0;
+//    public static boolean switchMove = false;
+
     public static double scaleTarget = 0;
+//    public static boolean scaleMove = false;
+
     public static double climbTarget = 0;
+//    public static boolean climbMove = false;
 
     /*
 
      */
 
 
-    public ElevatorSubsystem(int motor1, int motor2, int motor3, int motor4){
+    public ElevatorSubsystem(int leftElevator, int rightElevator){
 
-        this.motor1 = new TalonSRX(motor1);
-        this.motor2 = new TalonSRX(motor2);
-        this.motor3 = new TalonSRX(motor3);
-        this.motor4 = new TalonSRX(motor4);
+        this.leftElevator = new TalonSRX(leftElevator);
+        this.rightElevator = new TalonSRX(rightElevator);
 
-        this.pid = new PID(kP,kI,kD,kF);
+        this.leftElevator.set(ControlMode.Position, 0);
+        this.rightElevator.set(ControlMode.Follower, 0);
 
+        this.leftElevator.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Absolute, pidIdx, 20);
+        this.rightElevator.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Absolute, pidIdx, 20);
 
-//        this.motor2.set(ControlMode.Position, )
-//
-
+        this.leftElevator.config_kP(0, kP, 20);
+        this.leftElevator.config_kI(0, kI, 20);
+        this.leftElevator.config_kD(0, kD, 20);
+        this.leftElevator.config_kF(0, kF, 20);
 
     }
 
-    public void presets(){
-        motor2.set(ControlMode.Position, kP);
+    public void moveToPos() {
 
+//        while(groundMove){
+//            switchMove = false;
+//            scaleMove = false;
+//            climbMove = false;
+//
+//            leftElevator.set(ControlMode.Position, groundTarget);
+//            rightElevator.set(ControlMode.Follower, leftElevator.getDeviceID());
+//
+//
+//        }
+//
+//        while(switchMove){
+//            groundMove = false;
+//            scaleMove = false;
+//            climbMove = false;
+//
+//            leftElevator.set(ControlMode.Position, switchTarget);
+//            rightElevator.set(ControlMode.Follower, leftElevator.getDeviceID());
+//        }
 
+        switch(movePos){
 
+            case "switch":
+                leftElevator.set(ControlMode.Position, switchTarget);
+                rightElevator.set(ControlMode.Follower, leftElevator.getDeviceID());
+
+                if(leftElevator.getClosedLoopError(pidIdx) < 2 && leftElevator.getClosedLoopError(pidIdx) > -2){
+                    break;
+            }
+
+            case "ground":
+                leftElevator.set(ControlMode.Position, groundTarget);
+                rightElevator.set(ControlMode.Follower, leftElevator.getDeviceID());
+
+                if(leftElevator.getClosedLoopError(pidIdx) < 2 && leftElevator.getClosedLoopError(pidIdx) > -2){
+                    break;
+                }
+
+            case "scale":
+                leftElevator.set(ControlMode.Position, scaleTarget);
+                rightElevator.set(ControlMode.Follower, leftElevator.getDeviceID());
+
+                if(leftElevator.getClosedLoopError(pidIdx) < 2 && leftElevator.getClosedLoopError(pidIdx) > -2){
+                    break;
+                }
+
+            case "climb":
+                leftElevator.set(ControlMode.Position, climbTarget);
+                rightElevator.set(ControlMode.Follower, leftElevator.getDeviceID());
+
+                if(leftElevator.getClosedLoopError(pidIdx) < 2 && leftElevator.getClosedLoopError(pidIdx) > -2){
+                    break;
+                }
+
+            default:
+                leftElevator.set(ControlMode.Disabled, 0);
+                rightElevator.set(ControlMode.Disabled, 0);
+                break;
+        }
 
     }
 
