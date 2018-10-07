e18d92aa0c3f03f72a10590111dd92c9c343ea66
Justin Gonzales <jgonza1427@gmail.com>
2018-10-07 16:17:42 -0700

    Right Position Right Scale and Left Scale Functional
    Fixed Right Scale code and added (not perfect ) left scale code. All one cube.

--- MissCalculationCodeUpgrade/src/main/java/frc/team696/autonomousCommands/RightPos.java
+++ MissCalculationCodeUpgrade/src/main/java/frc/team696/autonomousCommands/RightPos.java
@@ -14,36 +14,49 @@ public class RightPos extends CommandGroup {
         if (gameData.length() > 0) {
             if (gameData.charAt(1) == 'R') {
 
+                //October 7, 2018, works! probably wont work during beach blitz though so...
+
                 addSequential(new ZeroElevator());
                 addSequential(new ZeroEncoders());
                 addSequential(new ZeroYaw());
                 addParallel(new Rise());
-                addSequential(new DriveCommand(270, 0, 0.85, 0.1));
-                addSequential(new Wait(1));
-                addSequential(new DriveCommand(270, -45, 0.25, 0.45), 1);
-                addSequential(new Wait(1));
-                addSequential(new DriveCommand(273, -45, 0.35, 0.35), 1);
+                addSequential(new DriveCommand(250, 0, 0.85, 0.1));
+                addSequential(new DriveCommand(250, -45, 0.25, 0.45), 1);
                 addSequential(new Tilt());
-//            addSequential(new Wait(-1));
                 addSequential(new MoveToPos("high"));
-//            addSequential(new Wait(1));
-                addSequential(new Outtake(1));
-//            addParallel(new OpenIntake());
-                addSequential(new OpenIntake());
-                addSequential(new Wait(1));
+                addSequential(new DriveCommand(265, -45, 0.45, 0.35), 0.5 );
+
+                addSequential(new Outtake(0.5), 0.3);
+
+                addParallel(new DriveCommand(250, -45, 0.45, 0.1), 0.5);
                 addSequential(new MoveToPos("intake"));
 
             }
 
             if (gameData.charAt(1) == 'L') {
 
+                //roughly works. good path. bad timing. 
+
                 addSequential(new ZeroElevator());
                 addSequential(new ZeroEncoders());
                 addSequential(new ZeroYaw());
                 addSequential(new Rise());
-                addSequential(new DriveCommand(135, 0, 1, 0.1));
+                addSequential(new DriveCommand(260, 0, 0.9, 0.1));
+                addSequential(new DriveCommand(260, -90, 0.3, 0.7));
+                addSequential(new DriveCommand(490, -90, 0.85, 0.1));
+                addSequential(new DriveCommand(490, 15, 0.3, 0.7));
+                addSequential(new Tilt());
+                addParallel(new MoveToPos("high"));
+                addSequential(new DriveCommand(530, 15, 0.55, 0.1));
+                addSequential(new Outtake(0.5), 1);
+                addSequential(new MoveToPos("intake"));
+                addSequential(new DriveCommand(520, 15, 0.5, 0.1));
+
+                
+
 
             }
+
         }
     }
 
